2eda381a7e43cf2959ccd8f18d995f4caad880da
ateyaf <ateyafkareem2@gmail.com>
2021-08-09 12:48:41 +0200

    php-exersies1&2

---
+++ Day1_Classwork.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="styel.css">
+    <title>Document</title>
+
+</head>
+<body>
+    <style></style>
+        
+    
+<?php 
+// # first exersies Day_1
+// $name = "Ateyaf & Kristofer";
+// echo "<h1> $name </h1>";
+
+
+
+#second exersise
+$name = "Ateyaf & Kristofer";
+$age = 30;
+$job_title ="couch driver";
+
+echo "<h1> Hi, we are  $name, and we are $age years old, and we works as a $job_title </h1>";
+
+echo'<img style= "width: 18%; height: 8%; margin-left: 35%; " src="https://cdn.pixabay.com/photo/2020/04/01/01/25/smile-4989709__340.png" > ';
+
+
+
+?>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+</body>
+</html>
